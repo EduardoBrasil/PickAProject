b991ba8506d38e39949c9942a27d501384efd17b
EduardoBrasil <brasil.eduardo1@gmail.com>
2015-10-14 09:50:04 -0300

    tool analysis static code

---
+++ db/migrate/20151010005135_add_user_id_to_project.rb
@@ -0,0 +1,8 @@
+class AddUserIdToProject < ActiveRecord::Migration
+  def self.up
+    add_column :projects, :user_id, :integer
+  end
+  def self.down
+  	remove_column :projects, :user_id
+  end
+end
