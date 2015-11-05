768bc3fecb81de002fb0d3a72e28152f20b09796
Luan Guimarães <luan@live.in>
2015-11-05 03:20:19 -0200

    Remove Project Task Model.

--- app/models/project_task.rb
+++
@@ -1,2 +0,0 @@
-class ProjectTask < ActiveRecord::Base
-end
--- db/migrate/20151105005635_add_picture_field_at_task_model.rb
+++
@@ -1,9 +0,0 @@
-# This class should add a new collumn in the Task table @ database
-class AddPictureFieldAtTaskModel < ActiveRecord::Migration
-	def up
-    add_attachment :projects, :image_file
-  end
-  def down
-    remove_attachment :projects, :image_file
-  end
-end
