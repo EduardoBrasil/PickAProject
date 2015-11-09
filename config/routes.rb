8f7692b36610d4b41db0ba098cfa4995cb4ba2c8
EduardoBrasil <brasil.eduardo1@gmail.com>
2015-11-09 19:14:34 -0200

    adding static_page about

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :projects
   resources :users
   root 'pages#home'
+
+  get 'about'=> 'pages#about'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
