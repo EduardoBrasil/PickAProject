72f4daacad68118dbf85c101e8aacdf287f692e8
EduardoBrasil <brasil.eduardo1@gmail.com>
2015-11-10 10:22:04 -0200

    create page help

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   resources :users
   root 'pages#home'
 
+  get 'help'=> 'pages#help'
   get 'about'=> 'pages#about'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
