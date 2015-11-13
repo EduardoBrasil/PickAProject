33860c6eb823b0c43e1f1f4606c646b71f8fd034
Luan Guimarães <luan@live.in>
2015-11-13 05:16:32 -0200

    Fix an error with the tasks permissions.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,28 +1,20 @@
 class ProjectsController < ApplicationController
   before_action :set_project, only: [:show, :edit, :update, :destroy]
 
-  # GET /projects
-  # GET /projects.json
   def index
     @projects = Project.all
   end
 
-  # GET /projects/1
-  # GET /projects/1.json
   def show
   end
 
-  # GET /projects/new
   def new
     @project = Project.new
   end
 
-  # GET /projects/1/edit
   def edit
   end
 
-  # POST /projects
-  # POST /projects.json
   def create
     @project = Project.new(project_params)
 
@@ -41,8 +33,6 @@ class ProjectsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /projects/1
-  # PATCH/PUT /projects/1.json
   def update
     respond_to do |format|
       if @project.update(project_params)
@@ -55,8 +45,6 @@ class ProjectsController < ApplicationController
     end
   end
 
-  # DELETE /projects/1
-  # DELETE /projects/1.json
   def destroy
     @project.destroy
     respond_to do |format|
@@ -74,7 +62,7 @@ class ProjectsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def project_params
       params.require(:project).permit(:title, :category, :level, :description, :status, :image_file,
-      tasks_attributes: [:id, :title, :description, :difficult, :_destroy])
+      tasks_attributes: [:id, :title, :description, :difficult, :image_file, :_destroy])
     end
 
 end
