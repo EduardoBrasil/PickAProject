7ca6ef6d7af604dbc1fd8f3c41c169b4fa9122ea
duerno <felipeduernoal@gmail.com>
2015-11-13 00:13:24 -0200

    Add index:true in parameters 'belongs_to' of migrations

--- db/migrate/20151112230000_create_project_members.rb
+++ db/migrate/20151112230000_create_project_members.rb
@@ -1,8 +1,8 @@
 class CreateProjectMembers < ActiveRecord::Migration
   def change
     create_table :project_members do |t|
-      t.belongs_to :projects
-      t.belongs_to :member, class: 'User'
+      t.belongs_to :projects, index: true
+      t.belongs_to :member, class: 'User', index: true
     end
   end
 end
