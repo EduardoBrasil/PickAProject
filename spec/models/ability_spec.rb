76fcd2d19cc7582235afdcc6cbef19e3160ff9b0
duerno <felipeduernoal@gmail.com>
2015-11-23 19:15:12 -0200

    Add private methods to create instances of users and projects for 'Ability' model tester

--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -6,4 +6,29 @@ require "cancan/matchers"
 require 'rails_helper'
 
 describe Ability do
+
+	# Methods below are used to create instances of users and projects
+	private
+
+		# Create User with default parameters, only valid fields
+		def create_user(options={})
+			User.create({
+				email: "example@email.com",
+				password: "password",
+				password_confirmation: "password"
+			}.merge(options))
+		end
+
+		# Create Project with default parameters, only valid fields
+		def create_project(options={})
+			Project.create({
+				title: "Title example",
+				level: "Easy",
+				category: "Art",
+				description: "Description example"*20,
+				status: "Active",
+				percentage: 50
+			}.merge(options))
+		end
+
 end
