94a19a621b792a21f19a171960dc81f16f34a63f
Luan Guimarães <luan@live.in>
2015-11-25 13:32:49 -0200

    Add some tests to the Project model.

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Project, type: :model do
   context "when providing valid data" do
     before :each do
-      @project = create_project
+      @project = create_project_with_task
     end
 
     # Testing the valid project generator
@@ -16,24 +16,115 @@ RSpec.describe Project, type: :model do
       another_project = @project.dup
       expect{
         another_project.save
-        
       }.to change(Project, :count).by(1)
+    end
+
+    it "should have a new task" do
+      expect{
+        @project.tasks << create_task
+      }.to change(@project.tasks, :count).by(1)
     end
   end
 
   context "when providing invalid data" do
     it "should not increase the database" do
-      project = create_project({
+      project = create_project_with_task(project_options: {
         title: "a"*(Project::TITLE_MAX_LENGTH + 1)
       })
       expect{
         project.save
       }.to change(Project, :count).by(0)
     end
+
+    it "should not be valid without a task" do
+      project = create_project
+      expect(project).not_to be_valid
+    end
+
+    context "when testing wrong titles" do
+      it "should no be valid without a title" do
+        project = create_project_with_task
+        # Manually adding of nil param, there is no way
+        # to do it with hash's merge method
+        project.title = nil
+        expect(project).not_to be_valid
+      end
+
+      it "should not be valid with too short title" do
+        project = create_project_with_task(project_options: {
+          title: "a"*(Project::TITLE_MIN_LENGTH - 1)
+        })
+        expect(project).not_to be_valid
+      end
+
+      it "should not be valid with too long title" do
+        project = create_project_with_task(project_options: {
+          title: "a"*(Project::TITLE_MAX_LENGTH + 1)
+        })
+        expect(project).not_to be_valid
+      end
+    end
+
+    context "when testing wrong descriptions" do
+      it "should no be valid without a description" do
+        project = create_project_with_task
+        # Manually adding of nil param, there is no way
+        # to do it with hash's merge method
+        project.description = nil
+        expect(project).not_to be_valid
+      end
+
+      it "should not be valid with too short description" do
+        project = create_project_with_task(project_options: {
+          description: "a"*(Project::DESCRIPTION_MIN_LENGTH - 1)
+        })
+        expect(project).not_to be_valid
+      end
+
+      it "should not be valid with too long description" do
+        project = create_project_with_task(project_options: {
+          title: "a"*(Project::DESCRIPTION_MAX_LENGTH + 1)
+        })
+        expect(project).not_to be_valid
+      end
+    end
+
+    context "when testing wrong percentage values" do
+      it "should not be valid without percentage value" do
+        project = create_project_with_task
+        # Manually adding of nil param, there is no way
+        # to do it with hash's merge method
+        project.percentage = nil
+        expect(project).not_to be_valid
+      end
+
+      it "should not be valid with non numerical characters" do
+        project = create_project_with_task(project_options: {
+          percentage: "a"
+        })
+      end
+
+      it "should not be valid with stupid percentage values" do
+        project = create_project_with_task(project_options: {
+          percentage: 101
+        })
+        expect(project).not_to be_valid
+      end
+
+      it "should not be valid with stupid percentage values" do
+        project = create_project_with_task(project_options: {
+          percentage: -1
+        })
+        expect(project).not_to be_valid
+      end
+    end
+
+
   end
 
   private
-  # Method to create some valid and writable Projects objects
+  # Method to create some valid and writable Projects params
+  # This must be invalid because there is no registered task
   def create_project(options = {})
     project = Project.create({
       title: "a"*(Project::TITLE_MIN_LENGTH),
@@ -48,14 +139,22 @@ RSpec.describe Project, type: :model do
   end
 
   # Create a valid task to validate the project
-  def get_task_params(options = {})
-    task_params = {
+  def create_task(options = {})
+    task = Task.create({
       title: "a"*(Task::TITLE_MIN_LENGTH),
       difficult: Task::PERMITTED_FIBONACCI_VALUES.first,
       description: "a"*(Task::DESCRIPTION_MIN_LENGTH)
-    }.merge(options)
+    }.merge(options))
 
-    return task_params
+    return task
   end
 
+  # Create a valid project with a task
+  def create_project_with_task(project_options: {}, task_options: {})
+    project = create_project(project_options)
+    task = create_task(task_options)
+    project.tasks << task
+
+    return project
+  end
 end
